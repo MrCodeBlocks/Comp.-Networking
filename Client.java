6e9e9d930ea0052aa634516e25114b1993d25769
Stevan Sanchez <55167754+MrCodeBlocks@users.noreply.github.com>
2020-03-12 19:01:50 -0500

    Client Code

---
+++ Client.java
@@ -0,0 +1,62 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+
+	private Socket socket = null;
+	private DataInputStream input = null;
+	private DataOutputStream out = null;
+	
+	public Client(String address, int port)
+	{ 
+		try
+		{
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+			
+			input = new DataInputStream(System.in);
+			out = new DataOutputStream(socket.getOutputStream());
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+			
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+		
+		String line = "";
+		
+		while(!line.contentEquals("Over"))
+		{
+			try
+			{
+				line = input.readLine();
+				out.writeUTF(line);
+			}
+			catch(IOException i)
+			{
+				System.out.println(i);
+			}
+		}
+		
+		try
+		{
+			input.close();
+			out.close();
+			socket.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("To close connection, enter: 'Over'");
+		Client client = new Client("localhost", 5001);
+	}
+
+}
